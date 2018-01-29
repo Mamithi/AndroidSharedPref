b4775cd320c89fc066ae2da1bec9020f97772058
Mamithi <19lawrence93@gmail.com>
2018-01-29 18:56:19 +0300

    Read and write data into shared preferences

--- app/src/main/java/com/mamithi/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/mamithi/sharedpreferences/MainActivity.java
@@ -1,13 +1,88 @@
 package com.mamithi.sharedpreferences;
 
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+   Toolbar mToolbar;
+   Button mRedColour;
+   Button mGreenColour;
+   Button mYellowColour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mRedColour = (Button) findViewById(R.id.btnRed);
+        mGreenColour = (Button) findViewById(R.id.btnGreen);
+        mYellowColour = (Button) findViewById(R.id.btnYellow);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mToolbar.setTitle(getResources().getString(R.string.app_name));
+        }
+
+        if(getColor() != getResources().getColor(R.color.colorPrimary)){
+            mToolbar.setBackgroundColor(getColor());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                getWindow().setStatusBarColor(getColor());
+            }
+        }
+
+        mRedColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.colorRed));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorRed));
+                }
+                storeColor(getResources().getColor(R.color.colorRed));
+            }
+        });
+
+        mGreenColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.colorGreen));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorGreen));
+                }
+                storeColor(getResources().getColor(R.color.colorGreen));
+            }
+        });
+
+        mYellowColour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.colorYellow));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.colorYellow));
+                }
+                storeColor(getResources().getColor(R.color.colorYellow));
+            }
+        });
+    }
+
+    private void storeColor(int color){
+        SharedPreferences mSharedPreferences = getSharedPreferences("ToolbarColor", MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+        mEditor.putInt("color", color);
+        mEditor.apply();
+    }
+
+    private int getColor(){
+        SharedPreferences mSharedPreferences = getSharedPreferences("ToolbarColor", MODE_PRIVATE);
+        int selectedColor = mSharedPreferences.getInt("color", getResources().getColor(R.color.colorPrimary));
+        return selectedColor;
+
     }
 }
